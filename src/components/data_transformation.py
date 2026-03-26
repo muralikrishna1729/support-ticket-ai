50209c2f3cc7cd6db12868f2bd9f6c572e09e348
muralikrishna1729 <muralikrishana1729@gmail.com>
2026-03-26 15:41:38 +0530

    EDA complete

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -7,6 +7,16 @@ from sklearn.preprocessing import OneHotEncoder,StandardScaler
 from src.exception import CustomException
 from src.logger import logging
 
-
+@dataclass
 class DataTransformationConfig:
-    
+    preprocesser_obj_file_path = os.path.join("artifacts",'preprocesser.pkl')
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config= DataTransformationConfig()
+
+    def data_transformation_object(self):
+        try:
+
+        except:
+              
